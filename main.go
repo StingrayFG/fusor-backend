fba71d317006c01a11b603fa26313ba694943d21
StingrayFG <54187585+StingrayFG@users.noreply.github.com>
2024-02-04 03:56:16 +0300

    Update record/get endpoints to query db

--- main.go
+++ main.go
@@ -1,29 +1,63 @@
 package main
 
 import (
-  //"fmt"
+  "fmt"
+  "log"
+  "os"
+
+  //"github.com/teris-io/shortid"
+  "github.com/joho/godotenv"
+
   "net/http"
   "github.com/gin-gonic/gin"
+  "github.com/gin-contrib/cors"
+
+  "gorm.io/driver/mysql"
+  "gorm.io/gorm"
 )
+
 
 type Record struct {
   Uuid          string  `json:"uuid"`
   OriginalLink  string  `json:"originalLink"`
 }
 
-var rec = Record{Uuid: "1", OriginalLink: "1"}
 
 func getRecord(c *gin.Context) {
-  //fmt.Println(rec)
-  c.IndentedJSON(http.StatusOK, rec)
+  err := godotenv.Load()
+  if err != nil {
+    log.Fatal("Error loading .env file")
+  }
+
+  db, err := gorm.Open(mysql.Open(os.Getenv("DATABASE_URL")), &gorm.Config{})
+  if err != nil {
+    log.Fatal("Failed to connect database")
+  }
+
+
+  record := Record{}
+  db.First(&record, "uuid = ?", c.Param("uuid"))
+  
+  fmt.Print(record)
+  if (Record{} != record) {
+    c.IndentedJSON(http.StatusOK, record)
+  } else {
+    c.IndentedJSON(http.StatusNotFound, Record{})
+  }
 }
 
 func main() {
   router := gin.Default()
+
   router.ForwardedByClientIP = true
   router.SetTrustedProxies([]string{"127.0.0.1"})
 
-  router.GET("/record/get", getRecord)
+  config := cors.DefaultConfig()
+  config.AllowOrigins = []string{"http://localhost:4200"}
+  router.Use(cors.New(config))
+  
+
+  router.GET("/record/get/:uuid", getRecord)
 
   router.Run("localhost:5200")
 }
